0698254040722f54ca79cb11c853c30ad6d2b2d6
osame001 <osame001@users.noreply.github.com>
2015-04-09 20:41:31 -0700

    hello

--- week01/ex3.py
+++ week01/ex3.py
@@ -1,28 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
 h = 800 # height in meter
 
 g = 9.81 # gravity aaceleration
 
 #vi = input("enter initial velocity=") #initial velocity
 
-vmin = input("enter minimum velocity=")
+vmin = float(input("enter minimum velocity="))
 
-vmax = input("enter maximum velocity=") 
+vmax = float(input("enter maximum velocity=")) 
 
-dx = ( vmax -vmin )/9
+dx = ( vmax -vmin )/10.
 
-s = (  vmin  + (( vmin )**2 + 2*g*h)**0.5 )/g
+t = np.zeros(10)
+vel_vector = vmin +np.arange(10).astype('float') * dx
+t = ( vel_vector  +( vel_vector**2 +2*g*h)**0.5)/g
+#for i in range(10): 
+    #vel_vector[i] = vmin + float(i)*dx
+    #t[i] = ( vel_vector[i]  +( vel_vector[i]**2 +2*g*h)**0.5)/g
 
-print " time for minimum velocity is+" , s
-  
-i= 0
+print "time is=" , t , "for velocity=" , vel_vector
 
-while i<9:
-    vmin = vmin + dx
-     
-    i = i + 1 
-    print " velocity=" , vmin
-    
-    t = (  vmin  + (( vmin )**2 + 2*g*h)**0.5 )/g
 
-    print "time of flight is=" , t
-
+plt.plot(vel_vector , t , marker="o" , linestyle="-" , color="b")
+plt.ylabel("time")
+plt.xlabel("initial velocity")
+plt.title("t=(V_initial +(V_initial**2+2*g*h)**0.5))/g")
+plt.savefig("ex3.jpeg" , format="jpeg")
